cd933bcdf7a178e2c4f211d870bb2f6b6681b93a
werkplek_2 <manuel.jager@x-guard.nl>
2021-06-25 14:27:03 +0200

    Use slack message API

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,3 +1,20 @@
+import Axios from 'axios';
+const ax = Axios.create();
+import { webhookUrl } from '../secrets';
+import { sendMessage } from '../src/main';
+
+
 describe('TODO - Add a test suite', () => {
-  it('TODO - Add a test', async () => {});
+  it('TODO - Add a test', async () => {
+    await sendMessage(webhookUrl, {
+      env: 'production',
+      envUrl: 'https://github.com/X-Guard/deployment-status/releases/tag/1.2.0',
+      state: 'failure',
+      repoUrl: 'https://github.com/X-Guard/deployment-status',
+      repoName: 'deployment-status',
+      refName: '#dd59568',
+      refUrl: 'https://github.com/X-Guard/deployment-status/commit/dd595687fee8fcd78cb5c7a2255236302f6d1da5',
+      logUrl: 'https://github.com/X-Guard/deployment-status/actions/runs/970630124',
+    })
+  });
 });
